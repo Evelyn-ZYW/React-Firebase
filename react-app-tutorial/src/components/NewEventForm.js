8b8a6c2da52f723ff4729d3383bc1df58dbb944e
Evelyn Z <evelynzdragon@gmail.com>
2021-11-10 12:45:09 -0800

    adding a dropdown list

--- react-app-tutorial/src/components/NewEventForm.js
+++ react-app-tutorial/src/components/NewEventForm.js
@@ -1,59 +1,69 @@
-// import { useState } from "react";
-import { useRef } from "react";
+import { useState } from "react";
+// import { useRef } from "react";
 import "./NewEventForm.css";
 
 export default function NewEventForm({ addEvent }) {
-  //   const [title, setTitle] = useState("");
-  //   const [date, setDate] = useState("");
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [location, setLocation] = useState("");
 
-  const title = useRef();
-  const date = useRef();
+  //   const title = useRef();
+  //   const date = useRef();
   const handleReset = () => {
-    // setTitle("");
-    // setDate("");
-    title.current.value = '';
-    date.current.value = '';
+    setTitle("");
+    setDate("");
+    // title.current.value = '';
+    // date.current.value = '';
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // const event = {
-    //   title: title,
-    //   date: date,
-    //   id: Math.floor(Math.random() * 10000),
-    // };
-    // addEvent(event);
-    // handleReset();
-
     const event = {
-      title: title.current.value,
-      date: date.current.value,
+      title: title,
+      date: date,
+      location: location,
       id: Math.floor(Math.random() * 10000),
     };
-
     addEvent(event);
+    console.log(event)
     handleReset();
-  };
 
+    // const event = {
+    //   title: title.current.value,
+    //   date: date.current.value,
+    //   id: Math.floor(Math.random() * 10000),
+    // };
+  };
+  const handleLocation = (e) => {
+    setLocation(e.target.value)
+  };
   return (
     <form className="new-event-form" onSubmit={handleSubmit}>
       <label>
         <span>Event Title:</span>
         <input
           type="text"
-          //   onChange={(e) => setTitle(e.target.value)}
-          //   value={title}
-          ref={title}
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          //   ref={title}
         ></input>
       </label>
       <label>
         <span>Event Date:</span>
         <input
           type="date"
-          //   onChange={(e) => setDate(e.target.value)}
-          //   value={date}
-          ref={date}
+          onChange={(e) => setDate(e.target.value)}
+          value={date}
+          //   ref={date}
         ></input>
+      </label>
+      <label>
+        <span>Event Location:</span>
+        <select onChange={handleLocation}>
+          <option value="manchester">Manchester</option>
+          <option value="london">London</option>
+          <option value="cardiff">Cardiff</option>
+        </select>
       </label>
       <button>Submit</button>
     </form>
