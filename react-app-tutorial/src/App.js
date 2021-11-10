14f68f5a41d9930194cd3cc819fdda3ecbf9b4ec
Evelyn Z <evelynzdragon@gmail.com>
2021-11-10 12:29:06 -0800

    useRef for input

--- react-app-tutorial/src/App.js
+++ react-app-tutorial/src/App.js
@@ -1,35 +1,32 @@
-import './App.css'
-import React, { useState } from 'react'
-import Title from './components/Title'
-import Modal from './components/Modal'
-import EventList from './components/EventList'
-import NewEventForm from './components/NewEventForm'
+import "./App.css";
+import React, { useState } from "react";
+import Title from "./components/Title";
+import Modal from "./components/Modal";
+import EventList from "./components/EventList";
+import NewEventForm from "./components/NewEventForm";
 
 function App() {
-  const [showModal, setShowModal] = useState(false)
-  const [showEvents, setShowEvents] = useState(true)
-  const [events, setEvents] = useState([
-    {title: "mario's birthday bash", id: 1},
-    {title: "bowser's live stream", id: 2},
-    {title: "race on moo moo farm", id: 3}
-  ])
+  const [showModal, setShowModal] = useState(false);
+  const [showEvents, setShowEvents] = useState(true);
+  const [events, setEvents] = useState([]);
+
+  const addEvent = (event) => {
+    setEvents((prev) => [...prev, event]);
+    setShowModal(false);
+  };
 
   const handleClick = (id) => {
-    setEvents(prevEvents => {
-      return prevEvents.filter(event => id !== event.id)
-    })
-  }
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => id !== event.id);
+    });
+  };
 
-  const handleClose = () => {
-    setShowModal(false)
-  }
-
-  const subtitle = "All the latest events in Marioland"
+  const subtitle = "All the latest events in Marioland";
 
   return (
     <div className="App">
       <Title title="Marioland Events" subtitle={subtitle} />
-      
+
       {showEvents && (
         <div>
           <button onClick={() => setShowEvents(false)}>Hide Events</button>
@@ -43,8 +40,8 @@ function App() {
       {showEvents && <EventList events={events} handleClick={handleClick} />}
 
       {showModal && (
-        <Modal handleClose={handleClose} isSalesModal={true}>
-          <NewEventForm />
+        <Modal isSalesModal={true}>
+          <NewEventForm addEvent={addEvent} />
         </Modal>
       )}
 
