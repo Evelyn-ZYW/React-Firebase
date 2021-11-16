b7902fb1718799c1b15ea1ca731a4a462fee2d63
Evelyn Z <evelynzdragon@gmail.com>
2021-11-15 17:36:00 -0800

    cleanup function is to avoid memory leak; it will always run when the component using this useEffect hook unmounts

--- tripago/src/hooks/useFetch.js
+++ tripago/src/hooks/useFetch.js
@@ -6,13 +6,14 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const controller = new AbortController();
     const fetchData = async () => {
       setIsPending(true);
 
       try {
-        const res = await fetch(url);
-        if(!res.ok){
-          throw new Error(res.statusText)
+        const res = await fetch(url, { signal: controller.signal });
+        if (!res.ok) {
+          throw new Error(res.statusText);
         }
         const json = await res.json();
 
@@ -20,12 +21,20 @@ const useFetch = (url) => {
         setData(json);
         setError(null);
       } catch (err) {
-        setIsPending(false);
-        setError("Could not fetch the data");
-        console.log(err.message);
+        if (err.name === "AbortError") {
+          console.log("the fetch was aborted");
+        } else {
+          setIsPending(false);
+          setError("Could not fetch the data");
+          console.log(err.message);
+        }
       }
     };
     fetchData();
+
+    return () => {
+      controller.abort();
+    };
   }, [url]);
   return { data, isPending, error };
 };
