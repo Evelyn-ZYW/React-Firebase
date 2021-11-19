294be8a48f69c1d5b97021e3467de42fb724f7ee
Evelyn Z <evelynzdragon@gmail.com>
2021-11-19 10:43:59 -0800

    was trying to use useHistory for redirect but didn't work; used useNavigate instead and worked

--- multipage-site/src/pages/Article.js
+++ multipage-site/src/pages/Article.js
@@ -1,10 +1,21 @@
+import { useEffect } from "react";
 import useFetch from "../hooks/useFetch";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 export default function Article() {
   const { id } = useParams();
   const url = `http://localhost:3000/articles/${id}`;
   const { data: article, isPending, error } = useFetch(url);
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        navigate("/");
+      }, 2000);
+    }
+  }, [error, navigate]);
+
   return (
     <div>
       {isPending && <div>Loading</div>}
