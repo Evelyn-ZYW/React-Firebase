5d4527fb162d1585953fdc2135e0d54262b75f9f
Evelyn Z <evelynzdragon@gmail.com>
2021-11-22 15:42:11 -0800

    understanding useReducer in React(simple example - change background color): 1. onClick event fires the color change function associated with the event. 2. the dispatch function inside of the color change function fires. 3. the reducer function associated with the dispatch function fires and do a switch statement ...

--- cooking-ninja/src/components/Navbar.js
+++ cooking-ninja/src/components/Navbar.js
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import Searchbar from "./Searchbar";
+import { useTheme } from "../hooks/useTheme";
 
 export default function Navbar() {
+  const { color, changeColor } = useTheme();
   return (
-    <div className="navbar">
+    <div
+      className="navbar"
+      onClick={() => changeColor("pink")}
+      style={{ backgroundColor: color }}
+    >
       <nav>
         <Link to="/" className="brand">
           <h1>Cooking Ninja</h1>
