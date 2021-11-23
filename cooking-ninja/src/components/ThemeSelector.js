96acc58a4bb03d20762d39bbdeecd5d74c406cd8
Evelyn Z <evelynzdragon@gmail.com>
2021-11-23 12:32:32 -0800

    mode icon color changes when mode changes

--- cooking-ninja/src/components/ThemeSelector.js
+++ cooking-ninja/src/components/ThemeSelector.js
@@ -5,12 +5,22 @@ import "./ThemeSelector.css";
 const themeColors = ["#58249c", "#249c6b", "#b70233"];
 
 export default function ThemeSelector() {
-  const { changeColor } = useTheme();
+  const { changeColor, changeMode, mode } = useTheme();
+
+  const toggleMode = () => {
+    changeMode(mode === "light" ? "dark" : "light");
+  };
+  console.log(mode);
 
   return (
     <div className="theme-selector">
       <div className="mode-toggle">
-        <img src={modeIcon} alt="dark/light toggle icon" />
+        <img
+          src={modeIcon}
+          alt="dark/light toggle icon"
+          onClick={toggleMode}
+          style={{ filter: mode === "dark" ? "invert(100%)" : "invert(20%)" }}
+        />
       </div>
       <div className="theme-buttons">
         {themeColors.map((color) => (
