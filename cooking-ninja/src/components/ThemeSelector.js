3b39832ed2928ecd28f8b2c156a995bd09789a42
Evelyn Z <evelynzdragon@gmail.com>
2021-11-23 12:17:22 -0800

    added theme mode icon and styles

--- cooking-ninja/src/components/ThemeSelector.js
+++ cooking-ninja/src/components/ThemeSelector.js
@@ -1,14 +1,17 @@
 import { useTheme } from "../hooks/useTheme";
-
+import modeIcon from "../assets/mode-icon.svg";
 import "./ThemeSelector.css";
 
 const themeColors = ["#58249c", "#249c6b", "#b70233"];
 
 export default function ThemeSelector() {
   const { changeColor } = useTheme();
-  
+
   return (
     <div className="theme-selector">
+      <div className="mode-toggle">
+        <img src={modeIcon} alt="dark/light toggle icon" />
+      </div>
       <div className="theme-buttons">
         {themeColors.map((color) => (
           <div
