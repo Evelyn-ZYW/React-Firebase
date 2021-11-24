b731fedcf9cadcf2623f9d66272f9d4c2f632f23
Evelyn Z <evelynzdragon@gmail.com>
2021-11-23 16:18:03 -0800

    fetch data with firestore database

--- cooking-ninja/src/pages/home/Home.js
+++ cooking-ninja/src/pages/home/Home.js
@@ -1,18 +1,44 @@
-import useFetch from "../../hooks/useFetch";
+// import useFetch from "../../hooks/useFetch";
+import { projectFirestore } from "../../firebase/config";
+import { useState, useEffect } from "react";
 import "./Home.css";
 
-import RecipeList from '../../components/RecipeList';
+import RecipeList from "../../components/RecipeList";
 export default function Home() {
-  const {
-    data,
-    isPending,
-    error,
-  } = useFetch("http://localhost:3000/recipes");
+  // const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
+
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsPending(true);
+
+    projectFirestore
+      .collection("recipes")
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          setError("No recipes to load :(");
+        } else {
+          let results = [];
+          snapshot.docs.forEach((doc) => {
+            results.push({ id: doc.id, ...doc.data() });
+          });
+          setData(results);
+          setIsPending(false);
+        }
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsPending(false);
+      });
+  }, []);
   return (
     <div className="home">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading</p>}
-      {data && <RecipeList recipes={data}/>}
+      {data && <RecipeList recipes={data} />}
     </div>
   );
 }
--- cooking-ninja/src/pages/recipe/Recipe.js
+++ cooking-ninja/src/pages/recipe/Recipe.js
@@ -1,30 +1,54 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import useFetch from "../../hooks/useFetch";
+// import useFetch from "../../hooks/useFetch";
 import { useTheme } from "../../hooks/useTheme";
+import { projectFirestore } from "../../firebase/config";
 import "./Recipe.css";
 export default function Recipe() {
   const { mode } = useTheme();
   const { id } = useParams();
-  const {
-    data: recipe,
-    isPending,
-    error,
-  } = useFetch(`http://localhost:3000/recipes/${id}`);
+
+  // const {
+  //   data: recipe,
+  //   isPending,
+  //   error,
+  // } = useFetch(`http://localhost:3000/recipes/${id}`);
+
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsPending(false);
+    projectFirestore
+      .collection("recipes")
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          setIsPending(false);
+          setData(doc.data());
+        } else {
+          setIsPending(false);
+          setError("could not find that recipe");
+        }
+      });
+  }, [id]);
 
   return (
     <div className={`recipe ${mode}`}>
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {recipe && (
+      {data && (
         <>
-          <h2 className="page-title">{recipe.title}</h2>
-          <p>Takes {recipe.cookingTime} to cook.</p>
+          <h2 className="page-title">{data.title}</h2>
+          <p>Takes {data.cookingTime} to cook.</p>
           <ul>
-            {recipe.ingredients.map((ing) => (
+            {data.ingredients.map((ing) => (
               <li key={ing}>{ing}</li>
             ))}
           </ul>
-          <p className="method">{recipe.method}</p>
+          <p className="method">{data.method}</p>
         </>
       )}
     </div>
