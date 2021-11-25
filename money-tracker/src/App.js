43fdd25a361ca8c91e2c58260cf06b767cb98475
Evelyn Z <evelynzdragon@gmail.com>
2021-11-24 22:59:30 -0800

    created Mavbar comp

--- money-tracker/src/App.js
+++ money-tracker/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home.js";
 import Login from "./pages/login/Login.js";
 import Signup from "./pages/signup/Signup.js";
+import Navber from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navber />
         <Routes>
           <Route exact path="/" element={<Home />} />
         </Routes>
