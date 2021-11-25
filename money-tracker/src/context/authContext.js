fbcafb551300812f4db49452069f1c73b50afc5b
Evelyn Z <evelynzdragon@gmail.com>
2021-11-25 13:57:57 -0800

    created useLogout hook and dispath a logout action

--- money-tracker/src/context/authContext.js
+++ money-tracker/src/context/authContext.js
@@ -6,13 +6,15 @@ export const authReducer = (state, action) => {
   switch (action.type) {
     case "LOGIN":
       return { ...state, user: action.payload };
+    case "LOGOUT":
+      return { ...state, user: null };
     default:
       return state;
   }
 };
 export function AuthContextProvider({ children }) {
   const [state, dispatch] = useReducer(authReducer, { user: null });
-  console.log("AuthContext state: ", state)
+  console.log("AuthContext state: ", state);
 
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
--- money-tracker/src/hooks/useLogout.js
+++ money-tracker/src/hooks/useLogout.js
@@ -1,4 +1,27 @@
-import {useState} from 'react';
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+import { projectAuth } from "../firebase/config";
+
 export const useLogout = () => {
-    return
-}
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const logout = async () => {
+    setError(null);
+    setIsPending(true);
+
+    try {
+      await projectAuth.signOut();
+      dispatch({ type: "LOGOUT" });
+      setIsPending(false);
+      setError(null);
+    } 
+    catch (err) {
+      console.log(err.message);
+      setError(err.message);
+      setIsPending(false);
+    }
+  };
+  return { error, isPending, logout };
+};
