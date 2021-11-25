a8a1db7afd2d0a84e684dfd2eb61dc5e2a2c9f63
Evelyn Z <evelynzdragon@gmail.com>
2021-11-24 23:35:53 -0800

    set up firebase/firestore config file

---
+++ money-tracker/src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAawfPSFP3wxKDVY7cdw5ON2fQDHe1dlHQ",
+  authDomain: "money-tracker-5c17a.firebaseapp.com",
+  projectId: "money-tracker-5c17a",
+  storageBucket: "money-tracker-5c17a.appspot.com",
+  messagingSenderId: "670562853757",
+  appId: "1:670562853757:web:08c5fc8bc2ac3eef0ca214",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
