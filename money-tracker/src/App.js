6f96c700d5eb18394321668ee911144f71d3c75d
Evelyn Z <evelynzdragon@gmail.com>
2021-11-24 16:18:58 -0800

    setting up react router, and page routes

--- money-tracker/src/App.js
+++ money-tracker/src/App.js
@@ -1,11 +1,26 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home/Home.js";
+import Login from "./pages/login/Login.js";
+import Signup from "./pages/signup/Signup.js";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
