fc6e26e0b32961679e3b6d52f76ba739d8564cc9
Evelyn Z <evelynzdragon@gmail.com>
2021-11-26 15:16:24 -0800

    protecting pages by conditionally routing based on user logged in or not

--- money-tracker/src/App.js
+++ money-tracker/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
@@ -8,16 +9,29 @@ import Navber from "./components/Navbar";
 import "./App.css";
 
 function App() {
+  const { authIsReady, user } = useAuthContext();
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navber />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navber />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
