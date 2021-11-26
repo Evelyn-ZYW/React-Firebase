6c67f59bac38ffca5bf209d9273c5f8be8ab678d
Evelyn Z <evelynzdragon@gmail.com>
2021-11-25 17:44:12 -0800

    watch auth state change

--- money-tracker/src/context/authContext.js
+++ money-tracker/src/context/authContext.js
@@ -1,4 +1,5 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
+import { projectAuth } from "../firebase/config";
 
 export const AuthContext = createContext();
 
@@ -8,14 +9,27 @@ export const authReducer = (state, action) => {
       return { ...state, user: action.payload };
     case "LOGOUT":
       return { ...state, user: null };
+    case "AUTH_IS_READY":
+      return { ...state, user: action.payload, authIsReady: true };
     default:
       return state;
   }
 };
 
 export function AuthContextProvider({ children }) {
-  const [state, dispatch] = useReducer(authReducer, { user: null });
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+    authIsReady: false,
+  });
+
   console.log("AuthContext state: ", state);
+
+  useEffect(() => {
+    const unsub = projectAuth.onAuthStateChanged((user) => {
+      dispatch({ type: "AUTH_IS_READY", payload: user });
+      unsub();
+    });
+  }, []);
 
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
