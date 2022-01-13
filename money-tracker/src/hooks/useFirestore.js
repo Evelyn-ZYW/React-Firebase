76e4808357b84fc9ec2009f7acaf5b6390422c42
Evelyn Z <evelynzdragon@gmail.com>
2022-01-12 23:29:33 -0800

    useFirestore hook to add/delete document; timestamp

--- money-tracker/src/hooks/useFirestore.js
+++ money-tracker/src/hooks/useFirestore.js
@@ -1,5 +1,5 @@
 import { useReducer, useState, useEffect } from "react";
-import { projectFirestore } from "../firebase/config";
+import { projectFirestore, timestamp } from "../firebase/config";
 
 let initialState = {
   document: null,
@@ -10,6 +10,22 @@ let initialState = {
 
 const firestoreReducer = (state, action) => {
   switch (action.type) {
+    case "IS_PENDING":
+      return { isPending: true, document: null, success: false, error: null };
+    case "ERROR":
+      return {
+        document: null,
+        isPending: false,
+        error: action.payload,
+        success: false,
+      };
+    case "ADDED_DOCUMENT":
+      return {
+        document: action.payload,
+        isPending: false,
+        error: null,
+        success: true,
+      };
     default:
       return state;
   }
@@ -22,11 +38,34 @@ export const useFirestore = (collection) => {
   // collection reference
   const ref = projectFirestore.collection(collection);
 
+  // re-usable dispatch function: when isCancelled is false
+  const dispatchIfNotCancelled = (action) => {
+    if (!isCancelled) {
+      dispatch(action);
+    }
+  };
+
   // add a document
-  const addDocument = async (doc) => {};
+  const addDocument = async (doc) => {
+    dispatch({ type: "IS_PENDING" });
+
+    try {
+      const createdAt = timestamp.fromDate(new Date());
+      const addedDocument = await ref.add({ ...doc, createdAt });
+      dispatchIfNotCancelled({
+        type: "ADDED_DOCUMENT",
+        payload: addedDocument,
+      });
+    } catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: err.message });
+    }
+  };
 
   // delete a document
-  const deleteDocument = async (id) => {};
+  const deleteDocument = async (id) => {
+    if (!isCancelled) {
+    }
+  };
 
   useEffect(() => {
     return () => setIsCancelled(true);
