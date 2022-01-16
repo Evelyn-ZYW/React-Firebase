b7e3bb6d1bbe85c36f4b593b1c1a285fd4292262
Evelyn Z <evelynzdragon@gmail.com>
2022-01-15 23:38:02 -0800

    format

--- money-tracker/src/pages/home/TransactionList.js
+++ money-tracker/src/pages/home/TransactionList.js
@@ -9,13 +9,17 @@ export default function TransactionList({ transactions }) {
   return (
     <ul className={styles.transactions}>
       <h3>My Transactions</h3>
-      {transactions.length ? transactions.map((transaction) => (
-        <li key={transaction.id}>
-          <p className={styles.name}>{transaction.name}</p>
-          <p className={styles.amount}>${transaction.amount}</p>
-          <button onClick={() => deleteDocument(transaction.id)}>X</button>
-        </li>
-      )):<p className={styles.nothing}>😯Empty...try add a transaction!</p>}
+      {transactions.length ? (
+        transactions.map((transaction) => (
+          <li key={transaction.id}>
+            <p className={styles.name}>{transaction.name}</p>
+            <p className={styles.amount}>${transaction.amount}</p>
+            <button onClick={() => deleteDocument(transaction.id)}>X</button>
+          </li>
+        ))
+      ) : (
+        <p className={styles.nothing}>😯Empty...try add a transaction!</p>
+      )}
     </ul>
   );
 }
