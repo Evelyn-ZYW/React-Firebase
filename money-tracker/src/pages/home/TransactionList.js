9c3ccf80302049a4d3db990deea4af1279b1ac78
Evelyn Z <evelynzdragon@gmail.com>
2022-01-15 23:35:28 -0800

    making everything else responsive - home, login. signup

--- money-tracker/src/pages/home/TransactionList.js
+++ money-tracker/src/pages/home/TransactionList.js
@@ -8,13 +8,14 @@ export default function TransactionList({ transactions }) {
   const { deleteDocument } = useFirestore("transactions");
   return (
     <ul className={styles.transactions}>
-      {transactions.map((transaction) => (
+      <h3>My Transactions</h3>
+      {transactions.length ? transactions.map((transaction) => (
         <li key={transaction.id}>
           <p className={styles.name}>{transaction.name}</p>
           <p className={styles.amount}>${transaction.amount}</p>
           <button onClick={() => deleteDocument(transaction.id)}>X</button>
         </li>
-      ))}
+      )):<p className={styles.nothing}>😯Empty...try add a transaction!</p>}
     </ul>
   );
 }
